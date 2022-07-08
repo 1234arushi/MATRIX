5ab671aae3841b086b08146e665220f6489de9f8
arushisharma <39919147+1234arushi@users.noreply.github.com>
2022-07-08 10:39:19 +0530

    Create 13.cpp

---
+++ GFG/13.cpp
@@ -0,0 +1,20 @@
+https://leetcode.com/problems/search-a-2d-matrix/submissions/
+class Solution {
+public:
+    bool searchMatrix(vector <vector<int>>& matrix, int target) {
+        
+        for(int i=0;i<matrix.size();i++)
+        {
+            for(int j=0;j<matrix[i].size();j++)
+            {
+                if(target==matrix[i][j])
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+        
+        
+    }
+};
